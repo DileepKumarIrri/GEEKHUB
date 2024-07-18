02e368a534462296a0fd115da845dc02d7b4e1ab
DileepKumarIrri <141381306+DileepKumarIrri@users.noreply.github.com>
2024-07-18 22:28:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Linked List/introduction-to-linked-list.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        // code here
+        if (arr.empty()) return nullptr; // Return nullptr if the vector is empty
+    
+    Node* head = new Node(arr[0]); // Create the head node with the first element
+    Node* temp = head;
+
+    for (size_t i = 1; i < arr.size(); ++i) {
+        temp->next = new Node(arr[i]); // Create a new node and link it to the current node
+        temp = temp->next; // Move to the next node
+    }
+
+    return head;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        Node* ans = obj.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
